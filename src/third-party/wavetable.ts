41323f7f65497445e4ec8b15063e7ac651e79de5
Luis Crespo <lcrespom@gmail.com>
2018-01-05 01:01:26 +0100

    Added instrument list

--- src/third-party/wavetable.ts
+++ src/third-party/wavetable.ts
@@ -294,3 +294,220 @@ function WebAudioFontPlayerConstructor() {
 
 	return this
 }
+
+/*
+Wavetable instrument list:
+
+0: Piano - Acoustic Grand Piano
+1: Piano - Bright Acoustic Piano
+2: Piano - Electric Grand Piano
+3: Piano - Honky-tonk Piano
+4: Piano - Electric Piano 1
+5: Piano - Electric Piano 2
+6: Piano - Harpsichord
+7: Piano - Clavinet
+8: Chromatic Percussion - Celesta
+9: Chromatic Percussion - Glockenspiel
+10: Chromatic Percussion - Music Box
+11: Chromatic Percussion - Vibraphone
+12: Chromatic Percussion - Marimba
+13: Chromatic Percussion - Xylophone
+14: Chromatic Percussion - Tubular Bells
+15: Chromatic Percussion - Dulcimer
+16: Organ - Drawbar Organ
+17: Organ - Percussive Organ
+18: Organ - Rock Organ
+19: Organ - Church Organ
+20: Organ - Reed Organ
+21: Organ - Accordion
+22: Organ - Harmonica
+23: Organ - Tango Accordion
+24: Guitar - Acoustic Guitar (nylon)
+25: Guitar - Acoustic Guitar (steel)
+26: Guitar - Electric Guitar (jazz)
+27: Guitar - Electric Guitar (clean)
+28: Guitar - Electric Guitar (muted)
+29: Guitar - Overdriven Guitar
+30: Guitar - Distortion Guitar
+31: Guitar - Guitar Harmonics
+32: Bass - Acoustic Bass
+33: Bass - Electric Bass (finger)
+34: Bass - Electric Bass (pick)
+35: Bass - Fretless Bass
+36: Bass - Slap Bass 1
+37: Bass - Slap Bass 2
+38: Bass - Synth Bass 1
+39: Bass - Synth Bass 2
+40: Strings - Violin
+41: Strings - Viola
+42: Strings - Cello
+43: Strings - Contrabass
+44: Strings - Tremolo Strings
+45: Strings - Pizzicato Strings
+46: Strings - Orchestral Harp
+47: Strings - Timpani
+48: Ensemble - String Ensemble 1
+49: Ensemble - String Ensemble 2
+50: Ensemble - Synth Strings 1
+51: Ensemble - Synth Strings 2
+52: Ensemble - Choir Aahs
+53: Ensemble - Voice Oohs
+54: Ensemble - Synth Choir
+55: Ensemble - Orchestra Hit
+56: Brass - Trumpet
+57: Brass - Trombone
+58: Brass - Tuba
+59: Brass - Muted Trumpet
+60: Brass - French Horn
+61: Brass - Brass Section
+62: Brass - Synth Brass 1
+63: Brass - Synth Brass 2
+64: Reed - Soprano Sax
+65: Reed - Alto Sax
+66: Reed - Tenor Sax
+67: Reed - Baritone Sax
+68: Reed - Oboe
+69: Reed - English Horn
+70: Reed - Bassoon
+71: Reed - Clarinet
+72: Pipe - Piccolo
+73: Pipe - Flute
+74: Pipe - Recorder
+75: Pipe - Pan Flute
+76: Pipe - Blown bottle
+77: Pipe - Shakuhachi
+78: Pipe - Whistle
+79: Pipe - Ocarina
+80: Synth Lead - Lead 1 (square)
+81: Synth Lead - Lead 2 (sawtooth)
+82: Synth Lead - Lead 3 (calliope)
+83: Synth Lead - Lead 4 (chiff)
+84: Synth Lead - Lead 5 (charang)
+85: Synth Lead - Lead 6 (voice)
+86: Synth Lead - Lead 7 (fifths)
+87: Synth Lead - Lead 8 (bass + lead)
+88: Synth Pad - Pad 1 (new age)
+89: Synth Pad - Pad 2 (warm)
+90: Synth Pad - Pad 3 (polysynth)
+91: Synth Pad - Pad 4 (choir)
+92: Synth Pad - Pad 5 (bowed)
+93: Synth Pad - Pad 6 (metallic)
+94: Synth Pad - Pad 7 (halo)
+95: Synth Pad - Pad 8 (sweep)
+96: Synth Effects - FX 1 (rain)
+97: Synth Effects - FX 2 (soundtrack)
+98: Synth Effects - FX 3 (crystal)
+99: Synth Effects - FX 4 (atmosphere)
+100: Synth Effects - FX 5 (brightness)
+101: Synth Effects - FX 6 (goblins)
+102: Synth Effects - FX 7 (echoes)
+103: Synth Effects - FX 8 (sci-fi)
+104: Ethnic - Sitar
+105: Ethnic - Banjo
+106: Ethnic - Shamisen
+107: Ethnic - Koto
+108: Ethnic - Kalimba
+109: Ethnic - Bagpipe
+110: Ethnic - Fiddle
+111: Ethnic - Shanai
+112: Percussive - Tinkle Bell
+113: Percussive - Agogo
+114: Percussive - Steel Drums
+115: Percussive - Woodblock
+116: Percussive - Taiko Drum
+117: Percussive - Melodic Tom
+118: Percussive - Synth Drum
+119: Percussive - Reverse Cymbal
+120: Sound effects - Guitar Fret Noise
+121: Sound effects - Breath Noise
+122: Sound effects - Seashore
+123: Sound effects - Bird Tweet
+124: Sound effects - Telephone Ring
+125: Sound effects - Helicopter
+126: Sound effects - Applause
+127: Sound effects - Gunshot
+
+Drums:
+Drum_Stan1_SC88P
+Standard
+Standard_part2
+Standard
+DRUM_SFX
+Room_2
+Standard_2_PART3
+CM_64_32_MT_32
+Room_3
+Roomm_PART3
+Room_4
+Power_PART3
+Room_5
+Electronic_PART3
+Room_6
+zTR_808_PART3
+Room_7
+Dance_PART3
+Power
+Jazz_PART3
+Power_1
+Brush_PART3
+Power_2
+Orchestra_PART3
+Power_3
+SFX_PART3
+Drum_Room
+Standard_1
+Roomm_PART2
+Room
+Electronic
+Standard
+TR_808
+Roomm
+Jazz
+Power_SC_55
+Jazz_1
+Electronic_SC_55
+Jazz_2
+zTR_808
+Jazz_3
+Dance_SC_88
+Jazz_4
+Jazz
+Brush
+Brush_SC_55
+Brush_1
+Orchestra
+Brush_2
+SFX
+Drum_Room_SC88P
+Standard_2
+Power_PART2
+Power
+Orchestra_Kit
+Drum_Power
+Standard_3
+Electronic_PART2
+Electronic
+Drum_Elec_SC88P
+Standard_4
+zTR_808_PART2
+TR_808
+Drum_TR808_SC88P
+Standard_5
+Dance_PART2
+Jazz
+Drum_TR909_SC88P
+Standard_6
+Jazz_PART2
+Brush
+Drum_Jazz
+Standard_7
+Brush_PART2
+Orchestra
+Drum_Brush_SC88P
+Room
+Orchestra_PART2
+SFX
+Drum_Orch_SC88P
+Room_1
+SFX_PART2
+*/
